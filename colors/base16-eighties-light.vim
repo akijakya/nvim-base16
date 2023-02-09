e29acc892bc1f942d803b520af63ae15b5c3bde1
András Jáky <jakyandras@gmail.com>
2023-02-09 10:12:58 +0100

    feat: darken some light colors of eighties-light

--- colors/base16-eighties-light.vim
+++ colors/base16-eighties-light.vim
@@ -2,6 +2,6 @@ let g:colors_name = 'base16-eighties-light'
 lua require('base16-colorscheme').setup({
     \base00 = '#f2f0ec', base01 = '#e8e6df', base02 = '#d3d0c8', base03 = '#a09f93',
     \base04 = '#747369', base05 = '#515151', base06 = '#393939', base07 = '#2d2d2d',
-    \base08 = '#f2777a', base09 = '#f99157', base0A = '#ffcc66', base0B = '#99cc99',
-    \base0C = '#66cccc', base0D = '#6699cc', base0E = '#cc99cc', base0F = '#d27b53'
+    \base08 = '#f2777a', base09 = '#f99157', base0A = '#f5c25c', base0B = '#8fc28f',
+    \base0C = '#5cc2c2', base0D = '#6699cc', base0E = '#cc99cc', base0F = '#d27b53'
     \})
--- lua/colors/eighties-light.lua
+++ lua/colors/eighties-light.lua
@@ -1,6 +1,18 @@
 return {
-    base00 = '#f2f0ec', base01 = '#e8e6df', base02 = '#d3d0c8', base03 = '#a09f93',
-    base04 = '#747369', base05 = '#515151', base06 = '#393939', base07 = '#2d2d2d',
-    base08 = '#f2777a', base09 = '#f99157', base0A = '#ffcc66', base0B = '#99cc99',
-    base0C = '#66cccc', base0D = '#6699cc', base0E = '#cc99cc', base0F = '#d27b53'
+	base00 = "#f2f0ec",
+	base01 = "#e8e6df",
+	base02 = "#d3d0c8",
+	base03 = "#a09f93",
+	base04 = "#747369",
+	base05 = "#515151",
+	base06 = "#393939",
+	base07 = "#2d2d2d",
+	base08 = "#f2777a",
+	base09 = "#f99157",
+	base0A = "#f5c25c", -- darkened by 10
+	base0B = "#8fc28f", -- darkened by 10
+	base0C = "#5cc2c2", -- darkened by 10
+	base0D = "#6699cc",
+	base0E = "#cc99cc",
+	base0F = "#d27b53",
 }
